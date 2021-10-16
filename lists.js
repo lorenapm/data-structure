ba1fd6601c4f3a6cae5fde55d9f7546c54f88f39
Lorena <lorena.perez@gmail.com>
2021-10-16 11:10:12 +0200

    create new list

--- lists.js
+++ lists.js
@@ -16,4 +16,17 @@ let nodo3 = new Node(9);
 nodo1.next = nodo2;
 nodo2.next = nodo3;
 
-console.log(nodo1);
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+}
+let myNode = new Node(5);
+let myNode2 = new Node(6);
+myNode.next = myNode2;
+let myList = new LinkedList(myNode);
+
+//acceder al primer elemento de mi lista
+console.log(myList.head);
+
+console.log(myList);
