9bb6097fa6783382bde96a0fa22c28b880f71818
Lorena <lorena.perez@gmail.com>
2021-10-16 12:17:01 +0200

    new list

--- lists.js
+++ lists.js
@@ -1,32 +1,56 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
 
 const array = [5, 6, 9, 2, 3];
 
-let nodo1 = new Node(5);
+// let nodo1 = new Node(5);
 
-let nodo2 = new Node(6);
+// let nodo2 = new Node(6);
 
-let nodo3 = new Node(9);
+// let nodo3 = new Node(9);
 
-nodo1.next = nodo2;
-nodo2.next = nodo3;
+// nodo1.next = nodo2;
+// nodo2.next = nodo3;
 
-class LinkedList {
-  constructor(head = null) {
-    this.head = head;
-  }
-}
-let myNode = new Node(5);
-let myNode2 = new Node(6);
-myNode.next = myNode2;
-let myList = new LinkedList(myNode);
+// class LinkedList {
+//   constructor(head = null) {
+//     this.head = head;
+//   }
+// }
+// let myNode = new Node(5);
+// let myNode2 = new Node(6);
+// myNode.next = myNode2;
+// let myList = new LinkedList(myNode);
 
 //acceder al primer elemento de mi lista
-console.log(myList.head);
+// console.log(myList.head);
 
-console.log(myList);
+// console.log(myList);
+
+//crear una lista a partir del array
+
+//crear una lista
+
+//1.creo la clase de cada node o item
+class LinkedListItem {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+//2.creo la lista que guardará el primer elemento de la lista, apuntando a null
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+let myNewItem = new LinkedListItem(3);
+
+let myNewList = new LinkedList(myNewItem);
+
+console.log(myNewList);
